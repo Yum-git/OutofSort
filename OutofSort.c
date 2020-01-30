a34271a056843f9f0953faae25c887f5c7b2b8dd
Yum-git <yzk.yuma@gmail.com>
2020-01-31 00:37:26 +0900

    2020/01/31

--- OutofSort.c
+++ OutofSort.c
@@ -2,70 +2,50 @@
 #include<stdlib.h>
 #include <string.h>
 #include<stdbool.h>
-
-
 #pragma warning(disable:4996)
-
 #define OutFileLimit 100000
-
 #include<time.h>
 #include<Windows.h>
 #include<process.h>
-
 #include<direct.h>
-
 #pragma comment(lib, "winmm.lib")
-
 #define SWAP(a, b) (tmp) = (a);(a) = (b);(b) = (tmp);
-
 #define M 32
 #define NSTACK 50
-
 int Foldernumber = 1;
 int Bool_1 = 0;
-
 void Outmerge(unsigned long i, unsigned long q);
-
 struct Merge_info {
-	unsigned long i;
-	unsigned long q;
+	unsigned long name;
+	unsigned long fold;
 };
-
-unsigned __stdcall Out_merge_ThreadEntry(void *arg) {
-	struct Merge_info *context = (struct Merge_info *)arg;
-	Outmerge(context->i, context->q);
+unsigned __stdcall Out_merge_ThreadEntry(void* arg) {
+	struct Merge_info* context = (struct Merge_info*)arg;
+	Outmerge(context->name, context->fold);
 	return 0;
 }
-
 void Outmerge(unsigned long n, unsigned long f) {
 	unsigned long name = n;
 	unsigned long Foldname = f;
 	char merge_first[20];
 	char merge_second[20];
 	char merge_result[20];
-
 	char Foldername[20];
-
-	FILE *mergeone;
-	FILE *mergetwo;
-	FILE *mergeresult;
+	FILE* mergeone;
+	FILE* mergetwo;
+	FILE* mergeresult;
 	int count = 0;
-
 	int checkleftcount = 0, checkrightcount = 0;
-
 	int F1 = 0, F2 = 0;
-
-	char *input_one = (char*)malloc(sizeof(char) * 100);
-	char *input_two = (char*)malloc(sizeof(char) * 100);
-
+	char* input_one = (char*)malloc(sizeof(char) * 100);
+	char* input_two = (char*)malloc(sizeof(char) * 100);
 	sprintf(merge_first, "dat%d\\%d.dat", Foldname, name);
 	sprintf(merge_second, "dat%d\\%d.dat", Foldname, name + 1);
 	sprintf(Foldername, "dat%d", Foldname + 1);
 	if (_mkdir(Foldername) == 0) {
 		printf("new folder\n");
 	}
-	sprintf(merge_result, "dat%d\\%d.dat", Foldname + 1 , name / 2 + name % 2);
-
+	sprintf(merge_result, "dat%d\\%d.dat", Foldname + 1, name / 2);
 	if ((mergeone = fopen(merge_first, "r")) == NULL) {
 		printf("file open error\n");
 		return;
@@ -76,21 +56,15 @@ void Outmerge(unsigned long n, unsigned long f) {
 			fprintf(mergeresult, "%s", input_one);
 		}
 		printf("second file open error\n");
-
 		fclose(mergeone);
 		fclose(mergeresult);
-
 		free(input_one);
 		free(input_two);
-
 		return;
 	}
-
 	mergeresult = fopen(merge_result, "w");
-
 	fgets(input_one, 100, mergeone);
 	fgets(input_two, 100, mergetwo);
-
 	for (;;) {
 		if (memcmp(input_one, input_two, 10) < 0) {
 			fprintf(mergeresult, "%s", input_one);
@@ -124,33 +98,25 @@ void Outmerge(unsigned long n, unsigned long f) {
 			break;
 		}
 	}
-
 	printf("Left: %d  Right: %d\n", checkleftcount, checkrightcount);
-
 	fclose(mergeone);
 	fclose(mergetwo);
 	fclose(mergeresult);
-
 	free(input_one);
 	free(input_two);
-
 	return;
 }
-
-
-
 struct thread_info {
-	char **arr;
+	char** arr;
 	unsigned long l;
 	unsigned long r;
 };
-
 void sort(unsigned long left, unsigned long right, char** arr) {
 	unsigned long i, j, k;
 	unsigned long l = left;
 	unsigned long r = right;
 	int* stack, nstack = 0;
-	char *a, *tmp;
+	char* a, *tmp;
 	stack = (int*)malloc(NSTACK * sizeof(int)) - 1;
 	for (;;) {
 		if (r - l < M) {
@@ -173,9 +139,9 @@ void sort(unsigned long left, unsigned long right, char** arr) {
 		else {
 			k = (l + r) >> 1;
 			SWAP(arr[k], arr[l + 1])
-			if (memcmp(arr[l + 1], arr[r], 10) > 0) {
-				SWAP(arr[l + 1], arr[r])
-			}
+				if (memcmp(arr[l + 1], arr[r], 10) > 0) {
+					SWAP(arr[l + 1], arr[r])
+				}
 			if (memcmp(arr[l], arr[r], 10) > 0) {
 				SWAP(arr[l], arr[r])
 			}
@@ -212,16 +178,13 @@ void sort(unsigned long left, unsigned long right, char** arr) {
 		}
 	}
 	free(stack + 1);
-
 }
-
-void merge(unsigned long p, unsigned long q, unsigned long r, char **arr) {
-
+void merge(unsigned long p, unsigned long q, unsigned long r, char** arr) {
 	printf("Merge in\n");
 	int i, k, j;
 	int n1 = q - p + 1, n2 = r - q;
-	char **aleft = (char**)malloc(sizeof(char) * n1 * 100);
-	char **aright = (char**)malloc(sizeof(char) * n2 * 100);
+	char** aleft = (char**)malloc(sizeof(char) * n1 * 100);
+	char** aright = (char**)malloc(sizeof(char) * n2 * 100);
 	for (i = 0; i < n1; i++) {
 		aleft[i] = (char*)malloc(sizeof(char) * 100);
 	}
@@ -242,59 +205,51 @@ void merge(unsigned long p, unsigned long q, unsigned long r, char **arr) {
 			arr[k + p] = aleft[i++];
 		}
 	}
-	
 	for (i = 0; i < n1; i++) {
 		free(aleft[i]);
 	}
 	for (i = 0; i < n2; i++) {
 		free(aright[i]);
 	}
-
 	free(aleft);
 	free(aright);
 	printf("Merge Out\n");
 }
-
-unsigned __stdcall qsortThreadEntry(void *arg) {
-	struct thread_info *context = (struct thread_info *)arg;
-	char **arr = context->arr;
+unsigned __stdcall qsortThreadEntry(void* arg) {
+	struct thread_info* context = (struct thread_info*)arg;
+	char** arr = context->arr;
 	sort(context->l, context->r, arr);
 	return 0;
 }
-
-unsigned __stdcall mergeThreadEntry(void *arg) {
-	struct thread_info *context = (struct thread_info *)arg;
-	char **arr = context->arr;
+unsigned __stdcall mergeThreadEntry(void* arg) {
+	struct thread_info* context = (struct thread_info*)arg;
+	char** arr = context->arr;
 	merge(context->l, (context->l + context->r) >> 1, context->r, arr);
 	return 0;
 }
-
-void sortQuickandMerge(unsigned long left, unsigned long right, char **arr) {
+void sortQuickandMerge(unsigned long left, unsigned long right, char** arr) {
 	unsigned long k;
 	unsigned long l = left;
 	unsigned long r = right;
-
 	unsigned long half1, half2;
-
 	HANDLE LeftMainThread, LeftSubThread, RightSubThread;
 	HANDLE LeftThread_merge;
 	struct thread_info parLeft1, parLeft2, parRight1;
 	struct thread_info parLeftmerge;
-
 	if (left >= right) {
 		exit(0);
 	}
-
-
 	sort(l, r, arr);
 	return;
 
-	/*下は現在未使用*/
 
+
+
+
+	/*下は現在未使用*/
 	k = (l + r) >> 1;
 	half1 = (l + k) >> 1;
 	half2 = (k + r) >> 1;
-
 	/*4分の1*/
 	parLeft1.l = l;
 	parLeft1.r = half1;
@@ -303,7 +258,7 @@ void sortQuickandMerge(unsigned long left, unsigned long right, char **arr) {
 		NULL,
 		0,
 		&qsortThreadEntry,
-		(void*)&parLeft1,
+		(void*)& parLeft1,
 		0,
 		NULL
 	);
@@ -312,7 +267,6 @@ void sortQuickandMerge(unsigned long left, unsigned long right, char **arr) {
 		return;
 	}
 	/*4分の1　end*/
-
 	/*4分の2*/
 	parLeft2.l = half1 + 1;
 	parLeft2.r = k;
@@ -321,7 +275,7 @@ void sortQuickandMerge(unsigned long left, unsigned long right, char **arr) {
 		NULL,
 		0,
 		&qsortThreadEntry,
-		(void*)&parLeft2,
+		(void*)& parLeft2,
 		0,
 		NULL
 	);
@@ -330,7 +284,6 @@ void sortQuickandMerge(unsigned long left, unsigned long right, char **arr) {
 		return;
 	}
 	/*4分の2 end*/
-
 	/*4分の3*/
 	parRight1.l = k + 1;
 	parRight1.r = half2;
@@ -339,7 +292,7 @@ void sortQuickandMerge(unsigned long left, unsigned long right, char **arr) {
 		NULL,
 		0,
 		&qsortThreadEntry,
-		(void*)&parRight1,
+		(void*)& parRight1,
 		0,
 		NULL
 	);
@@ -348,19 +301,15 @@ void sortQuickandMerge(unsigned long left, unsigned long right, char **arr) {
 		return;
 	}
 	/*4分の3 end*/
-
 	/*4分の4*/
 	sort(half2 + 1, r, arr);
 	/*4分の4 end*/
-
 	WaitForSingleObject(LeftMainThread, INFINITE);
 	WaitForSingleObject(LeftSubThread, INFINITE);
 	WaitForSingleObject(RightSubThread, INFINITE);
-
 	CloseHandle(LeftMainThread);
 	CloseHandle(LeftSubThread);
 	CloseHandle(RightSubThread);
-
 	/*2分の1*/
 	parLeftmerge.l = l;
 	parLeftmerge.r = k;
@@ -369,7 +318,7 @@ void sortQuickandMerge(unsigned long left, unsigned long right, char **arr) {
 		NULL,
 		0,
 		&mergeThreadEntry,
-		(void*)&parLeftmerge,
+		(void*)& parLeftmerge,
 		0,
 		NULL
 	);
@@ -378,28 +327,30 @@ void sortQuickandMerge(unsigned long left, unsigned long right, char **arr) {
 		return;
 	}
 	/*2分の1 end*/
-
 	/*2分の2*/
 	merge(k + 1, (k + 1 + r) >> 1, r, arr);
 	/*2分の2 end*/
-
 	WaitForSingleObject(LeftThread_merge, INFINITE);
 	CloseHandle(LeftThread_merge);
-
 	printf("merge first in\n");
 	/*1分の1*/
 	merge(l, k, r, arr);
 	/*1分の1　end*/
-
 	printf("\n\n");
 
-	/*上は現在未使用*/
-}
 
+
+
+
+
+
+	/*上は現在未使用*/
+
+}
 int main() {
-	char *FileName = "Testof10000000.dat";
+	char* FileName = "160000000.dat";
 	char outname[20];
-	FILE *fp;
+	FILE* fp;
 	bool ExitCount = 0;
 	if ((fp = fopen(FileName, "r")) == NULL) {
 		printf("file open error\n");
@@ -409,9 +360,8 @@ int main() {
 	unsigned long OutLimitMane = 0;
 	i = 0;
 	while (1) {
-		char **input_array;
-		input_array = (char**)malloc(sizeof(char *) * OutFileLimit * 100);
-
+		char** input_array;
+		input_array = (char**)malloc(sizeof(char*) * OutFileLimit * 100);
 		for (i = 0; i < OutFileLimit; i++) {
 			input_array[i] = (char*)malloc(sizeof(char) * 100);
 		}
@@ -425,12 +375,11 @@ int main() {
 		if (ExitCount) {
 			break;
 		}
-		FILE *outfile;
+		FILE* outfile;
 		sprintf(outname, "%s\\%d.dat", "dat0", OutLimitMane);
 		outfile = fopen(outname, "w");
-		char **aa = input_array - 1;
+		char** aa = input_array - 1;
 		sortQuickandMerge(1, OutFileLimit, aa);
-
 		for (i = 0; i < OutFileLimit; i++) {
 			//printf("%d : %s", i + 1, input_array[i]);
 			fprintf(outfile, "%s", input_array[i]);
@@ -443,81 +392,77 @@ int main() {
 		printf("%d:End\n", OutLimitMane);
 		OutLimitMane++;
 	}
-
 	fclose(fp);
-
 	/*↑一時ファイル保存まで↑*/
-
 	/*↓それらをマージしていく↓*/
-
+	unsigned int foldroop = OutLimitMane;
 	int tmp = OutLimitMane;
 	for (i = 0; OutLimitMane != 1; i++) {
 		OutLimitMane = (OutLimitMane + 1) / 2;
+		foldroop = i;
 	}
+	foldroop++;
 
-	/*
-	printf("%d\n", i);
-	return 0;
-	*/
 
-	int p, q;
-	unsigned long par4Q = 0;
-	for (p = 0; p < OutLimitMane; p++) {
-		tmp = (tmp + 1) / 2;
-		for (q = 0; q < tmp; q++) {
+	int q;
+	unsigned long p;
+	unsigned long par4_name;
+
+	char removefile[20];
+	for (p = 0; p < foldroop; p++) {
+		for (q = 0; q < tmp;) {
 			HANDLE SecondThread, ThirdThread, FoursThread;
 			struct Merge_info par1, par2, par3;
-
-			par1.q = p;
-			par1.i = q;
-
-			par2.q = p;
-			par2.i = par1.i + 2;
-
-			par3.q = p;
-			par3.i = par2.i + 2;
-
-			printf("%d, %d, %d", par1.i, par2.i, par3.i);
-
+			par1.name = q;
+			par1.fold = p;
+			par2.name = par1.name + 2;
+			par2.fold = p;
+			par3.name = par2.name + 2;
+			par3.fold = p;
+			printf("%d, %d ", par1.name, par1.name + 1);
+			printf("%d, %d ", par2.name, par2.name + 1);
+			printf("%d, %d ", par3.name, par3.name + 1);
 			SecondThread = (HANDLE)_beginthreadex(
 				NULL,
 				0,
 				&Out_merge_ThreadEntry,
-				(void*)&par1,
+				(void*)& par1,
 				0,
 				NULL
 			);
-
 			ThirdThread = (HANDLE)_beginthreadex(
 				NULL,
 				0,
 				&Out_merge_ThreadEntry,
-				(void*)&par2,
+				(void*)& par2,
 				0,
 				NULL
 			);
-
 			FoursThread = (HANDLE)_beginthreadex(
 				NULL,
 				0,
 				&Out_merge_ThreadEntry,
-				(void*)&par3,
+				(void*)& par3,
 				0,
 				NULL
 			);
-
-			par4Q = par3.i + 2;
-			Outmerge(i, par4Q);
+			par4_name = par3.name + 2;
+			Outmerge(par4_name, p);
 			WaitForSingleObject(SecondThread, INFINITE);
 			WaitForSingleObject(ThirdThread, INFINITE);
 			WaitForSingleObject(FoursThread, INFINITE);
-
 			CloseHandle(SecondThread);
 			CloseHandle(ThirdThread);
 			CloseHandle(FoursThread);
-
+			q = par4_name + 2;
 		}
+		for (i = 0;; i++) {
+			sprintf(removefile, "dat%d\\%d.dat", p, i);
+			if (remove(removefile) != 0) {
+				break;
+			}
+		}
+		tmp = (tmp + 1) / 2;
 	}
-
 	return 0;
 }
